f73e0bd256a3d021f10ca7b5646a4b1b3d77d50e
thinker <thinker_ljn@163.com>
2018-02-09 23:29:53 +0800

    v0

---
+++ index.js
@@ -0,0 +1,33 @@
+const validate = (binding) => {
+  if (typeof binding.value !== 'function') {
+    console.warn('[Vue-outoffocus:] provided expression', binding.expression, 'is not a function.')
+    return false
+  }
+  return true
+}
+
+export default {
+  bind: function (el, binding) {
+    if (!validate(binding)) return
+
+    el.__vueOutOfFocusCb__ = (e) => {
+      if (!el.contains(e.relatedTarget)) setTimeout(binding.value, 0)
+    }
+
+    el.setAttribute('tabindex', '-1')
+    el.style.outline = 0
+
+    el.addEventListener('blur', el.__vueOutOfFocusCb__, true)
+  },
+  inserted: function (el) {
+    el.focus()
+  },
+  update: function (el, binding) {
+    if (!validate(binding)) return
+    el.focus()
+  },
+  unbind: function (el, binding) {
+    el.removeEventListener('blur', el.__vueOutOfFocusCb__, true)
+    delete el.__vueOutOfFocusCb__
+  }
+}

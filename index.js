c5c7f06cc43f52f38ab750307abc5e64a86f3e92
thinker <thinker_ljn@163.com>
2018-11-07 15:31:00 +0800

    update

--- index.js
+++ index.js
@@ -6,12 +6,43 @@ const validate = (binding) => {
   return true
 }
 
+const getObserverParams = function (el) {
+  let options = {
+    attributes: true,
+    attributeFilter: ['style', 'tabindex']
+  }
+  let callback = function (mutationList, observer) {
+    let oldDsp = observer.__oldDsp || ''
+    let dsp = el.style.display
+    let isDisplayed = dsp !== 'none' && oldDsp === 'none'
+    observer.__oldDsp = dsp
+
+    let tabindexMutation = mutationList.find(m => m.attributeName === 'tabindex')
+    if (tabindexMutation || isDisplayed) {
+      el.focus()
+    }
+  }
+
+  return {options, callback}
+}
+
 export default {
   bind: function (el, binding) {
     if (!validate(binding)) return
 
+    let cb = binding.value
+
+    let {options, callback} = getObserverParams(el)
+    let observer = new MutationObserver(callback)
+    observer.observe(el, options)
+    el.__observer__ = observer
+
     el.__vueOutOfFocusCb__ = (e) => {
-      if (!el.contains(e.relatedTarget) && !el.contains(document.activeElement)) setTimeout(binding.value, 0)
+      let activeEl = document.activeElement
+
+      if (activeEl !== el && !el.contains(activeEl)) {
+        setTimeout(cb, 0)
+      }
     }
 
     el.setAttribute('tabindex', '-1')
@@ -22,5 +53,7 @@ export default {
   unbind: function (el, binding) {
     el.removeEventListener('blur', el.__vueOutOfFocusCb__, true)
     delete el.__vueOutOfFocusCb__
+    el.__observer__.disconnect()
+    delete el.__observer__
   }
 }
